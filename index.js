e91ea2da9aa8e4ac32eea9ec34e69b00aa149bdb
blessing krofegha <bkrofegha@gmail.com>
2019-09-24 05:39:15 -1100

    setup dotenv

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+
+
+dotenv.config();
+
+
+//Connect to DB
+mongoose.connect(process.env.DB_CONNECT,
+  {useNewUrlParser: true },
+  () => console.log('Connected to db!!')
+);
 const app = express();
 
 //Import Routes
