9ee86caba59f0d149e7c25958a00673adbefea62
blessing krofegha <bkrofegha@gmail.com>
2019-10-15 06:28:26 -1100

    cleaned files

--- frontends/src/App.js
+++ frontends/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 // import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-import Home from './components/Home';
+import Home from '../src/components/Home/Home';
 
 class App extends Component {
 
@@ -11,7 +11,8 @@ class App extends Component {
   render() {
     return (
         <div className="App">
-          <h1> Hello wolrd </h1>
+        <h1> Hello wolrd </h1>
+        <Home />
           {/* <Router>
             <Switch>
               <Route exact path="/" component={Home} />
--- frontends/src/components/Home/Home.js
+++ frontends/src/components/Home/Home.js
@@ -3,10 +3,6 @@ import React, { Component } from 'react';
 
 class Home extends Component {
 
-  constructor(props) {
-    super(props);
-
-  }
 
 
 
