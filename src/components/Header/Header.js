56d3922e23aac54d62fb7f41df17d545680d41d6
blessing krofegha <bkrofegha@gmail.com>
2019-10-22 20:16:17 -1100

    add header hamburger

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,11 +4,11 @@
     render() {
         return (
         <div>
-        <div class="navbar-area">
-            <div class="luvion-responsive-nav">
-                <div class="container">
-                    <div class="luvion-responsive-menu">
-                        <div class="logo">
+        <div className="navbar-area">
+            <div className="luvion-responsive-nav">
+                <div className="container">
+                    <div className="luvion-responsive-menu">
+                        <div className="logo">
                             <a href="index-2.html">
                                 <img src="assets/img/logo.png" alt="logo"/>
                                 <img src="assets/img/black-logo.png" alt="logo"/>
@@ -18,75 +18,21 @@
                 </div>
             </div>
 
-            <div class="luvion-nav">
-                <div class="container">
-                    <nav class="navbar navbar-expand-md navbar-light">
-                        <a class="navbar-brand" href="index-2.html">
+            <div className="luvion-nav">
+                <div className="container">
+                    <nav className="navbar navbar-expand-md navbar-light">
+                        <a className="navbar-brand" href="index-2.html">
                             <img src="assets/img/logo.png" alt="logo"/>
                             <img src="assets/img/black-logo.png" alt="logo"/>
                         </a>
 
-                        <div class="collapse navbar-collapse mean-menu" id="navbarSupportedContent">
-                            <ul class="navbar-nav">
-                                <li class="nav-item"><a href="#" class="nav-link">Home <i class="fas fa-chevron-down"></i></a>
-                                    <ul class="dropdown-menu">
-                                        <li class="nav-item"><a href="index-2.html" class="nav-link active">Home One</a></li>
-
-                                        <li class="nav-item"><a href="index-3.html" class="nav-link">Home Two</a></li>
-
-                                        <li class="nav-item"><a href="index-4.html" class="nav-link">Home Three</a></li>
-                                    </ul>
+                        <div className="collapse navbar-collapse mean-menu" id="navbarSupportedContent">
+                            <ul className="navbar-nav">
+                                <li className="nav-item"><a href="/" className="nav-link">Home</a>
                                 </li>
-
-                                <li class="nav-item"><a href="about.html" class="nav-link">About Us</a></li>
-
-                                <li class="nav-item"><a href="#" class="nav-link">Features <i class="fas fa-chevron-down"></i></a>
-                                    <ul class="dropdown-menu">
-                                        <li class="nav-item"><a href="features-1.html" class="nav-link">Features Style One</a></li>
-
-                                        <li class="nav-item"><a href="features-2.html" class="nav-link">Features Style Two</a></li>
-                                    </ul>
-                                </li>
-
-                                <li class="nav-item"><a href="#" class="nav-link">Pages <i class="fas fa-chevron-down"></i></a>
-                                    <ul class="dropdown-menu">
-                                        <li class="nav-item"><a href="about.html" class="nav-link">About Us</a></li>
-
-                                        <li class="nav-item"><a href="#" class="nav-link">Features</a>
-                                            <ul class="dropdown-menu">
-                                                <li class="nav-item"><a href="features-1.html" class="nav-link">Features Style One</a></li>
-        
-                                                <li class="nav-item"><a href="features-2.html" class="nav-link">Features Style Two</a></li>
-                                            </ul>
-                                        </li>
-
-                                        <li class="nav-item"><a href="team.html" class="nav-link">Team</a></li>
-
-                                        <li class="nav-item"><a href="pricing.html" class="nav-link">Pricing</a></li>
-
-                                        <li class="nav-item"><a href="error-404.html" class="nav-link">404 Error</a></li>
-
-                                        <li class="nav-item"><a href="faq.html" class="nav-link">FAQ</a></li>
-                                    </ul>
-                                </li>
-
-                                <li class="nav-item"><a href="pricing.html" class="nav-link">Pricing</a></li>
-
-                                <li class="nav-item"><a href="#" class="nav-link">Blog <i class="fas fa-chevron-down"></i></a>
-                                    <ul class="dropdown-menu">
-                                        <li class="nav-item"><a href="blog-1.html" class="nav-link">Blog Grid</a></li>
-
-                                        <li class="nav-item"><a href="blog-2.html" class="nav-link">Blog Right Sidebar</a></li>
-
-                                        <li class="nav-item"><a href="single-blog.html" class="nav-link">Blog Details</a></li>
-                                    </ul>
-                                </li>
-
-                                <li class="nav-item"><a href="contact.html" class="nav-link">Contact</a></li>
                             </ul>
-
-                            <div class="others-options">
-                                <a href="login.html" class="login-btn"><i class="flaticon-user"></i> Log In</a>
+                            <div className="others-options">
+                                <a href="login.html" className="login-btn"><i className="flaticon-user"></i> Log In</a>
                             </div>
                         </div>
                     </nav>
