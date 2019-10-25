c7d3b331b554cf4ca14bd2e0aa6c298c5cfd3768
blessing krofegha <bkrofegha@gmail.com>
2019-10-24 22:50:59 -1100

    changed css file path

--- src/components/admin/css.php
+++ src/components/admin/css.php
@@ -1,11 +1,11 @@
 <link href="bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="plugins/bower_components/bootstrap-extension/css/bootstrap-extension.css" rel="stylesheet">
-    <!-- Menu CSS -->
-    <link href="plugins/bower_components/sidebar-nav/dist/sidebar-nav.min.css" rel="stylesheet">
- <link href="plugins/bower_components/toast-master/css/jquery.toast.css" rel="stylesheet">
-    <!-- animation CSS -->
-    <link href="css/animate.css" rel="stylesheet">
-    <!-- Custom CSS -->
-    <link href="css/style.css" rel="stylesheet">
-    <!-- color CSS -->
-    <link href="css/colors/gray-dark.css" id="theme" rel="stylesheet">
+   <link href="plugins/bower_components/bootstrap-extension/css/bootstrap-extension.css" rel="stylesheet">
+   <!-- Menu CSS -->
+   <link href="plugins/bower_components/sidebar-nav/dist/sidebar-nav.min.css" rel="stylesheet">
+<link href="plugins/bower_components/toast-master/css/jquery.toast.css" rel="stylesheet">
+   <!-- animation CSS -->
+   <link href="css/animate.css" rel="stylesheet">
+   <!-- Custom CSS -->
+   <link href="css/style.css" rel="stylesheet">
+   <!-- color CSS -->
+   <link href="css/colors/gray-dark.css" id="theme" rel="stylesheet">
